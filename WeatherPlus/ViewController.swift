9cf56da475a8e40f723289872390ee223ccfb130
Anatoli Shmanai <vintagemixing@gmail.com>
2020-12-08 19:46:19 +0300

    Start interface.

--- WeatherPlus/ViewController.swift
+++ WeatherPlus/ViewController.swift
@@ -8,12 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let maxHeaderHeight: CGFloat = 250
+    let minHeaderHeight: CGFloat = 80
+    var previousScrollOffset: CGFloat = 0
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var headerViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+//        let cell = UINib(nibName: "myCellTableViewCell", bundle: nil)
+//        tableView.register(cell, forCellReuseIdentifier: "myCell")
+
     }
-
-
 }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Wow!"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+}
+extension ViewController {
+    func canAnimateHeader (_ scrollView: UIScrollView) -> Bool {
+        let scrollViewMaxHeight = scrollView.frame.height + self.headerViewHeight.constant - minHeaderHeight
+        return scrollView.contentSize.height > scrollViewMaxHeight
+    }
+    func setScrollPosition() {
+        self.tableView.contentOffset = CGPoint(x:0, y: 0)
+    }
+}
+extension ViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollDiff = (scrollView.contentOffset.y - previousScrollOffset)
+        let isScrollingDown = scrollDiff > 0
+        let isScrollingUp = scrollDiff < 0
+        if canAnimateHeader(scrollView) {
+            var newHeight = headerViewHeight.constant
+            if isScrollingDown {
+                newHeight = max(minHeaderHeight, headerViewHeight.constant - abs(scrollDiff))
+            } else if isScrollingUp {
+                newHeight = min(maxHeaderHeight, headerViewHeight.constant + abs(scrollDiff))
+            }
+            if newHeight != headerViewHeight.constant {
+                headerViewHeight.constant = newHeight
+                setScrollPosition()
+                previousScrollOffset = scrollView.contentOffset.y
+            }
+        }
+    }
+}
+
+
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCollectionCell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+}

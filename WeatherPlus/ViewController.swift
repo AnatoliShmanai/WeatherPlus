984315ab3acdabe4d768d1e47624fead9d001918
Anatoli Shmanai <vintagemixing@gmail.com>
2020-12-08 18:38:48 +0300

    Initial Commit

---
+++ WeatherPlus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherPlus
+//
+//  Created by Anatoli Shmanai on 8.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

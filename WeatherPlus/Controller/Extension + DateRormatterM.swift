0dbe1417099f1c3bf17a23ed094a12d074218c85
Anatoli Shmanai <vintagemixing@gmail.com>
2020-12-11 22:13:47 +0300

    Added formatter file.

--- WeatherPlus/Controller/Extension + DateRormatterM.swift
+++ WeatherPlus/Controller/Extension + DateRormatterM.swift
@@ -1,11 +1,12 @@
 //
-//  Extension + DateFormatter.swift
+//  Extension + DateRormatterM.swift
 //  WeatherPlus
 //
 //  Created by Anatoli Shmanai on 11.12.20.
 //
 
 import Foundation
+
 
 extension ViewController {
     func getTimeForDate(_ date: Date?) -> String {

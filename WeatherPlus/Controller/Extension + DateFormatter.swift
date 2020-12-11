04c85d8cb33465d175aa25882bc9a49d4bcaaf1d
Anatoli Shmanai <vintagemixing@gmail.com>
2020-12-11 22:01:58 +0300

    /

---
+++ WeatherPlus/Controller/Extension + DateFormatter.swift
@@ -0,0 +1,21 @@
+//
+//  Extension + DateFormatter.swift
+//  WeatherPlus
+//
+//  Created by Anatoli Shmanai on 11.12.20.
+//
+
+import Foundation
+
+extension ViewController {
+    func getTimeForDate(_ date: Date?) -> String {
+        guard let inputDate = date else {
+            return ""
+        }
+
+        let formatter = DateFormatter()
+                formatter.dateFormat = "HH:mm"
+
+        return formatter.string(from: inputDate)
+    }
+}
